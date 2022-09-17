a6146621d3c6dc1f613bd156b646e7a593426658
manuxi <11303615+manuxi@users.noreply.github.com>
2022-09-17 02:40:41 +0200

    Update ManuxiGoogleReviewsBundle.php

--- src/ManuxiGoogleReviewsBundle.php
+++ src/ManuxiGoogleReviewsBundle.php
@@ -3,11 +3,12 @@
 namespace Manuxi\GoogleReviewsBundle;
 
 use Manuxi\GoogleReviewsBundle\DependencyInjection\ManuxiGoogleReviewsExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ManuxiGoogleReviewsBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new ManuxiGoogleReviewsExtension();
